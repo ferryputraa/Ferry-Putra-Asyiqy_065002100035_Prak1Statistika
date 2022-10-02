d9b4bb283e20e694ae3d8459ceb382faf0fb94c4
ferryputraa <91236219+ferryputraa@users.noreply.github.com>
2022-10-03 01:51:00 +0700

    Add files via upload

---
+++ PrakStatis 1.R
@@ -0,0 +1,14 @@
+data_ferry = read.delim("clipboard")
+View(data_ferry)
+mean(data_ferry$Tinggi.Badan)
+str(data_ferry)
+
+data_ferry_csv = read.csv("C:/Users/USER/Downloads/ferry_prak1.csv")
+View(data_ferry_csv)
+mean(data_ferry_csv$Tinggi.Badan)
+str(data_ferry_csv)
+
+install.packages("xlsx")
+library("xlsx")
+
+data_ferry_xlsx= read.xlsx("C:/Users/USER/Downloads/STATIS/ferry_prak1.xlsx",1)
